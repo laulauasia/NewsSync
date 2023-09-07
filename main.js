b29df1ed9342d418a9849921480ae637ccdc0d7a
Priyanshu Trivedi <priyanshutrivedi0081@gmail.com>
2023-09-07 12:22:29 +0530

    Add dark mode functionality, change logo and some minor fixes

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ window.addEventListener('load', () => fetchNews('India'));
 async function fetchNews(query){
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
     bindData(data.articles);
 }
 
@@ -75,3 +75,28 @@ hamIcon.addEventListener('click', () => {
         bars.classList.add('fa-bars');
     }
 })
+
+
+// for Dark Mode
+const toggleSwitch = document.getElementById('checkbox');
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+
+    if (currentTheme === "dark") {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+    }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
